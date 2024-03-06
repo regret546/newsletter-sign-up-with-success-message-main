d00bfeffda2ba8ae5e273523f05fb39174b6903a
regret546 <iregret546@gmail.com>
2024-03-06 23:31:23 +0800

    add input validation

--- app/js/script.js
+++ app/js/script.js
@@ -10,3 +10,30 @@ function changeImageSrc() {
 }
 changeImageSrc();
 window.addEventListener("resize", changeImageSrc);
+
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const userInput = document.querySelector("#emailInput");
+const submitBtn = document.querySelector("#submitBtn");
+const dismissBtn = document.querySelector("#dismissBtn");
+const emailInput = document.querySelector("#emailInput");
+
+function validateEmailInputs() {
+  const errorMessage = document.querySelector("#errorMsg");
+  const inputValue = emailInput.value;
+
+  if (inputValue === "") {
+    errorMessage.innerText = "Email must not be empty";
+    errorMessage.classList.add("errorMessageTrue");
+  } else if (!emailRegex.test(inputValue)) {
+    errorMessage.innerText = "Valid email is required";
+    errorMessage.classList.add("errorMessageTrue");
+  } else {
+    errorMessage.innerText = "";
+    errorMessage.classList.remove("errorMessageTrue");
+  }
+}
+
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  validateEmailInputs();
+});
